1d3f6996d0e19dce1b7f014116debc5c2274e254
FLYCOM-E <flycome9798@gmail.com>
2025-12-17 12:26:49 +0000

    Update: 新增多配置支持，旧配置不再受支持（升级需重写配置）

--- AutoDisableServer.c
+++ AutoDisableServer.c
@@ -6,72 +6,119 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <android/log.h>
 
-#define DEBUG 0
-#define WAITTIME 10
-#define MAX_PACKAGE 255
-#define CHECKLIST "CheckList.conf"
-#define DISABLELIST "DisableList.conf"
 #define GET_TOPAPP "dumpsys window | grep mCurrentFocus | head -n 1 | cut -f 1 -d '/' | cut -f 5 -d ' ' | cut -f 1 -d ' '"
+#define LOGPRINT __android_log_print
+#define MAX_PATH 4096
+#define MAX_PACKAGE 256
+#define MAX_CONFIG 10
+#define WAITTIME 10
 
-static int service(int mode, char * disablelist_file);
+static int service(int mode, char * config_dir, char * package);
 
-int main(int COMI, char * COM[])
+int main(int argc, char * argv[])
 {
     if (getuid() != 0 && getuid() != 2000) //检查当前用户权限
     {
         printf(" » Please Use Root or Shell Run\n");
         return 0;
     }
-    if (COMI < 2) //检查参数数量
+    if (argc < 2) //检查参数数量
     {
         printf(" » 未传入配置路径！\n");
         return 1;
     }
-    else if (access(COM[1], F_OK) != 0) //检查路径能否访问
+    else if (access(argv[1], F_OK) != 0) //检查路径能否访问
     {
         printf(" » 配置路径不可访问或不存在！\n");
         return 1;
     }
     
-    // 拼接完整配置文件路径并检查
-    char checklist_file[strlen(COM[1]) + 24];
-    char disablelist_file[strlen(COM[1]) + 24];
-    snprintf(checklist_file, sizeof(checklist_file), "%s/%s", COM[1], CHECKLIST);
-    snprintf(disablelist_file, sizeof(disablelist_file), "%s/%s", COM[1], DISABLELIST);
-    
-    // 检查 CheckList、DisableList存在性
-    if (access(checklist_file, F_OK) != 0)
+    // 遍历配置目录逐一读取保存声明包名
+    int file_count = 0;
+    char package_list[MAX_CONFIG][MAX_PACKAGE] = {0};
+    struct dirent * entry;
+    DIR * config_dir_dp = opendir(argv[1]);
+    if (config_dir_dp == NULL)
     {
-        printf(" » %s 配置不存在/名称错误！\n", CHECKLIST);
+        printf(" » 配置目录打开失败！\n");
         return 1;
     }
-    else if (access(disablelist_file, F_OK) != 0)
+    while ((entry = readdir(config_dir_dp)))
     {
-        printf(" » %s 配置不存在/名称错误！\n", DISABLELIST);
-        return 1;
-    }
-    
-    if (DEBUG == 0) // 非Debug模式则脱离控制端启动并忽略输出
-    {
-        pid_t PID = fork();
-        if (PID == -1)
+        if (strcmp(entry -> d_name, ".") == 0 ||
+           strcmp(entry -> d_name, "..") == 0)
         {
-            printf(" » 进程启动失败！\n");
-            return 1;
+            continue;
         }
-        else if (PID != 0)
-        {
-            exit(0);
-        }
-        setsid();
         
-        int std = open("/dev/null", O_RDWR);
-        dup2(std, STDIN_FILENO);
-        dup2(std, STDOUT_FILENO);
-        dup2(std, STDERR_FILENO);
-        close(std);
+        char config_path[MAX_PATH] = "", config_line[MAX_PACKAGE] = "";
+        snprintf(config_path, sizeof(config_path), "%s/%s", argv[1], entry -> d_name);
+        FILE * config_path_fp = fopen(config_path, "r");
+        if (config_path_fp == NULL)
+        {
+            printf(" » %s 配置打开失败，自动跳过！\n", entry -> d_name);
+            continue;
+        }
+        if (fgets(config_line, sizeof(config_line), config_path_fp))
+        {
+            /*
+            配置声明：@<包名>
+            */
+            config_line[strcspn(config_line, "\n")] = 0;
+            char * config_line_p = config_line;
+            while (isspace(* config_line_p)) config_line_p++;
+            
+            if (* config_line_p != '@')
+            {
+                printf(" » %s 配置声明错误，自动跳过！\n", entry -> d_name);
+                continue;
+            }
+            if (file_count <= MAX_CONFIG)
+            {
+                snprintf(package_list[file_count], sizeof(package_list[file_count]), "%s", config_line + 1);
+                printf(" » 已加载配置 %s，包名 %s\n", entry -> d_name, config_line + 1);
+                file_count++; // 已读取配置+1
+            }
+            else
+            {
+                printf(" » 加载配置数量超过限制，已加载 %d 个配置，跳过其它配置！\n", file_count);
+                break;
+            }
+        }
+        else
+        {
+            printf(" » %s 配置加载失败！已跳过\n", entry -> d_name);
+        }
+        fclose(config_path_fp);
     }
+    closedir(config_dir_dp);
+    
+    if (file_count == 0) // 未读取任何配置
+    {
+        printf(" » 配置目录为空！未读取任何配置\n");
+        return 1;
+    }
+    
+    pid_t PID = fork();
+    if (PID == -1)
+    {
+        printf(" » 进程启动失败！\n");
+        return 1;
+    }
+    else if (PID != 0)
+    {
+        exit(0);
+    }
+    setsid();
+    int std = open("/dev/null", O_RDWR);
+    dup2(std, STDIN_FILENO);
+    dup2(std, STDOUT_FILENO);
+    dup2(std, STDERR_FILENO);
+    close(std);
     
     //定义循环所需变量
     int mode = 0, max_get_topapp_error = 0;
@@ -94,10 +141,10 @@ int main(int COMI, char * COM[])
             }
             else if (max_get_topapp_error == 20)
             {
-                printf(" » Get TopApp Err. Timeout...\n");
+                LOGPRINT(ANDROID_LOG_ERROR, "AutoDisable", "Get TopApp Failed! Timeout...\n");
                 break;
             }
-            printf(" » Get TopApp Err. Continue\n");
+            LOGPRINT(ANDROID_LOG_WARN, "AutoDisable", "Get TopApp Failed! Again...\n");
             continue;
         }
         fgets(top_app, sizeof(top_app), top_app_fp);
@@ -111,7 +158,7 @@ int main(int COMI, char * COM[])
            strstr(top_app, "StatusBar") ||
            strstr(top_app, "ActionsDialog"))
         {
-            printf(" » Top is SystemUI. Wait\n");
+            LOGPRINT(ANDROID_LOG_INFO, "AutoDisable", "Top is SystemUI. Wait\n");
             sleep(WAITTIME);
             continue;
         }
@@ -122,7 +169,7 @@ int main(int COMI, char * COM[])
             // 这里检查当前前台是否未变化
             if (strcmp(top_app, old_top_app) == 0)
             {
-                printf(" » App Not Cycle. Wait\n");
+                LOGPRINT(ANDROID_LOG_INFO, "AutoDisable", "App Not Cycle. Wait\n");
                 sleep(WAITTIME);
                 continue;
             }
@@ -133,51 +180,46 @@ int main(int COMI, char * COM[])
         }
         
         // 读取检测列表并检测当前前台 App 是否在该列表
-        char checklist_line[MAX_PACKAGE] = "";
-        FILE * checklist_file_fp = fopen(checklist_file, "r");
-        if (checklist_file_fp)
+        int i = 0;
+        while (i < file_count)
         {
-            while (fgets(checklist_line, sizeof(checklist_line), checklist_file_fp))
+            if (strcmp(package_list[i], top_app) == 0)
             {
-                checklist_line[strcspn(checklist_line, "\n")] = 0;
-                if (strcmp(checklist_line, top_app) == 0)
+                if (mode == 2)
                 {
-                    /* 如果当前前台是被监测 App 则置 mode=1 并保留包名以检测变化
-                    这里不会影响前面置2的行为，因为如果前台仍然是被监测App就没必要解冻App列表 */
-                    mode = 1;
-                    snprintf(old_top_app, sizeof(old_top_app), "%s", checklist_line);
-                    break;
+                    service(2, argv[1], old_top_app);
                 }
+                /* 如果当前前台是被监测 App 则置 mode=1 并保留包名以检测变化
+                这里不会影响前面置2的行为，因为如果前台仍然是被监测App就没必要解冻App列表 */
+                mode = 1;
+                snprintf(old_top_app, sizeof(old_top_app), "%s", package_list[i]); // 保留包名（对应配置）
+                break;
             }
-            fclose(checklist_file_fp);
-        }
-        else
-        {
-            printf(" » %s Read Err\n", CHECKLIST);
-            break;
+            i++;
         }
         
-        // Service
-        if (service(mode, disablelist_file) == 0)
+        if (mode == 1)
         {
-            if (mode == 2) mode = 0; //解冻后重置此值
-        }
-        else
-        {
-            // 失败检查 CheckList、DisableList
-            if (access(checklist_file, F_OK) != 0)
+            int end = service(mode, argv[1], package_list[i]);
+            if (end != 0)
             {
-                printf(" » %s 配置已不存在！\n", CHECKLIST);
-                return 1;
+                LOGPRINT(ANDROID_LOG_WARN, "AutoDisable", "Error Code %d\n", end);
             }
-            else if (access(disablelist_file, F_OK) != 0)
+        }
+        else if (mode == 2)
+        {
+            int end = service(mode, argv[1], old_top_app);
+            if (end == 0)
             {
-                printf(" » %s 配置已不存在！\n", DISABLELIST);
-                return 1;
+                mode = 0; //解冻后重置此值
+            }
+            else
+            {
+                LOGPRINT(ANDROID_LOG_WARN, "AutoDisable", "Error Code %d\n", end);
             }
         }
         
-        printf(" » Cycle Wait\n");
+        LOGPRINT(ANDROID_LOG_INFO, "AutoDisable", "Cycle Wait.\n");
         sleep(WAITTIME);
     }
     return 0;
@@ -186,91 +228,104 @@ int main(int COMI, char * COM[])
 /*
 mode 为 1 则读取冻结 App 列表并逐一冻结
 mode 为 2 则读取冻结 App 列表并逐一解冻
-返回 0 代表成功，127 代表列表打开失败
+返回 0 成功 1 错误
 */
-static int service(int mode, char * disablelist_file)
-{
-    int end = 0;
-    char disablelist_line[MAX_PACKAGE] = "";
-    FILE * disablelist_file_fp = fopen(disablelist_file, "r");
-    if (disablelist_file_fp)
+static int service(int mode, char * config_dir, char * package)
+{    
+    // 定义模式对应pm参数，同时检查是否需要操作
+    char arg[64] = "";
+    if (mode == 1)
     {
-        if (mode == 1)
-        {
-            while (fgets(disablelist_line, sizeof(disablelist_line), disablelist_file_fp))
-            {
-                disablelist_line[strcspn(disablelist_line, "\n")] = 0;
-                pid_t newPid = fork();
-                if (newPid == -1)
-                {
-                    printf(" » DisableService Fork Error!\n");
-                    continue;
-                }
-                if (newPid == 0)
-                {
-                    execlp("pm", "pm", "disable-user", disablelist_line, NULL);
-                    _exit(1);
-                }
-                else
-                {
-                    int i = 0;
-                    if (waitpid(newPid, &i, 0) == -1)
-                    {
-                        printf(" » Wait DisableService Error!\n");
-                        continue;
-                    }
-                    if (WIFEXITED(i) && WEXITSTATUS(i) == 0)
-                    {
-                        printf(" » Disable: %s\n", disablelist_line);
-                    }
-                    else
-                    {
-                        printf(" » Disable: %s Err\n", disablelist_line);
-                    }
-                }
-            }
-        }
-        else if (mode == 2)
-        {
-            while (fgets(disablelist_line, sizeof(disablelist_line), disablelist_file_fp))
-            {
-                disablelist_line[strcspn(disablelist_line, "\n")] = 0;
-                pid_t newPid = fork();
-                if (newPid == -1)
-                {
-                    printf(" » EnableService Fork Error!\n");
-                    continue;
-                }
-                if (newPid == 0)
-                {
-                    execlp("pm", "pm", "enable", disablelist_line, NULL);
-                    _exit(1);
-                }
-                else
-                {
-                    int i = 0;
-                    if (waitpid(newPid, &i, 0) == -1)
-                    {
-                        printf(" » Wait EnableService Error!\n");
-                        continue;
-                    }
-                    if (WIFEXITED(i) && WEXITSTATUS(i) == 0)
-                    {
-                        printf(" » Enable: %s\n", disablelist_line);
-                    }
-                    else
-                    {
-                        printf(" » Enable: %s Err\n", disablelist_line);
-                    }
-                }
-            }
-        }
+        snprintf(arg, sizeof(arg), "disable-user");
     }
-    else
+    else if (mode == 2)
     {
-        end = 127;
+        snprintf(arg, sizeof(arg), "enable");
+    }
+    if (access(config_dir, F_OK) != 0)
+    {
+        return 1;
     }
     
-    fclose(disablelist_file_fp);
-    return end;
+    int end = 0;
+    struct dirent * entry;
+    DIR * config_dir_dp = opendir(config_dir);
+    if (config_dir_dp == NULL)
+    {
+        return 1;
+    }
+    while ((entry = readdir(config_dir_dp)))
+    {
+        if (strcmp(entry -> d_name, ".") == 0 ||
+           strcmp(entry -> d_name, "..") == 0)
+        {
+            continue;
+        }
+        if (end == 1)
+        {
+            break;
+        }
+        
+        int count = 0;
+        char config_path[MAX_PATH] = "", config_line[MAX_PACKAGE] = "";
+        snprintf(config_path, sizeof(config_path), "%s/%s", config_dir, entry -> d_name);
+        FILE * config_path_fp = fopen(config_path, "r");
+        if (config_path_fp == NULL)
+        {
+            continue;
+        }
+        while (fgets(config_line, sizeof(config_line), config_path_fp))
+        {
+            config_line[strcspn(config_line, "\n")] = 0;
+            if (count == 0)
+            {
+                char * config_line_p = config_line;
+                while (isspace(* config_line_p)) config_line_p++;
+                if (* config_line_p != '@')
+                {
+                    break;
+                }
+                if (strcmp(package, config_line + 1) != 0)
+                {
+                    break;
+                }
+                end = 1;
+                count++;
+                continue;
+            }
+            
+            pid_t newPid = fork();
+            if (newPid == -1)
+            {
+                LOGPRINT(ANDROID_LOG_WARN, "AutoDisable", "Fork Error! Skip.\n");
+                continue;
+            }
+            if (newPid == 0)
+            {
+                execlp("pm", "pm", arg, config_line, NULL);
+                _exit(1);
+            }
+            else
+            {
+                int i = 0;
+                if (waitpid(newPid, &i, 0) == -1)
+                {
+                    LOGPRINT(ANDROID_LOG_WARN, "AutoDisable", "Wait Error! Skip.\n");
+                    continue;
+                }
+                if (WIFEXITED(i) && WEXITSTATUS(i) == 0)
+                {
+                    LOGPRINT(ANDROID_LOG_INFO, "AutoDisable", "%s: %s Success.\n", arg, config_line);
+                }
+                else
+                {
+                    LOGPRINT(ANDROID_LOG_WARN, "AutoDisable", "%s: %s Error! \n", arg, config_line);
+                }
+            }
+        }
+        fclose(config_path_fp);
+    }
+    closedir(config_dir_dp);
+    
+    return (end == 1) ? 0 : 1;
 }
--- README.md
+++ README.md
@@ -1,28 +1,28 @@
    # 什么是 **Auto_Disable** ？
 
-**Auto_Disable** 是一个自动检测前台配置 **App** 并冻结指定 **App** 的模块。
+**Auto_Disable** 是一个自动检测前台配置 **App** 并冻结指定 **App** 的模块，支持多配置文件。
 
 
 
-配置文件填写：
+   # 编写自定义配置文件：
 
-检测 **App** 列表：**`CheckList.conf`**
+配置目录：模块根目录下 **CONFIG** 目录（此目录不强制绑定Server，但模块service.sh默认指定这个，如果您是自定义启动Server请忽略）
 
-一行一个包名，当模块监测到其中某软件处于前台运行状态则冻结以下列表内软件：
+新建文本文件，不限后缀
 
-冻结 **App** 列表：**`DisableList.conf`**
+第一行开头填写`@<检测App包名>`
 
-一行一个包名，当模块监测到 **`CheckList.conf`** 中某软件处于前台运行状态则冻结该列表内软件
-
-配置更改实时生效，无需重启设备/进程
+之后一行一个待冻结软件包名
 
 
-
-模块会自动检查前台软件，当监测到 **`CheckList.conf`** 内软件在前台运行时自动冻结 **`DisableList.conf`** 内填写的软件，当前台离开 **`CheckList.conf`** 监测的软件后自动解冻 **`DisableList.conf`** 内填写的软件
+**模块会自动检查前台软件，当监测到 **检测App** 在前台运行时自动冻结该列表内其它软件，
+当前台离开被检测软件后自动解冻**
 
 
 
-模块核心 **`AutoDisableServer`** 实际支持以 **Shell** 权限运行，将 **`AutoDisableServer`** 复制进 **`/data/local/tmp`** 使用 **`/data/local/tmp/AutoDisableServer <配置文件所在路径>`** 命令即可正常启动 **Server** 。暂未考虑做方便实现
+模块核心 **AutoDisableServer** 支持以 **Shell** 权限运行，将 **AutoDisableServer** 
+复制进 **`/data/local/tmp`** 使用 **`/data/local/tmp/AutoDisableServer <配置文件所在路径>`** 
+命令即可正常启动 **Server** 。暂未考虑做方便实现
 
 
 
